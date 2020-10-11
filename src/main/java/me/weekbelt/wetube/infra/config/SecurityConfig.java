43626b5bb7c443c89aa7f2e089d2998e36a5b865
weekbelt <vfrvfr4207@gmail.com>
2020-10-11 16:39:13 +0900

    feat: Add link

--- src/main/java/me/weekbelt/wetube/infra/config/SecurityConfig.java
+++ src/main/java/me/weekbelt/wetube/infra/config/SecurityConfig.java
@@ -13,7 +13,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
        http.authorizeRequests()
-               .mvcMatchers("/").permitAll()
+               .mvcMatchers("/", "/login", "/join", "search").permitAll()
                .anyRequest().authenticated();
     }
 
