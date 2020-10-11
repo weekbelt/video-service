d3b92657c5b4fa2c5d23a2888a04273af86e7377
weekbelt <vfrvfr4207@gmail.com>
2020-10-11 14:19:23 +0900

    Initial commit

---
+++ src/main/java/me/weekbelt/wetube/WetubeApplication.java
@@ -0,0 +1,13 @@
+package me.weekbelt.wetube;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WetubeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WetubeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/weekbelt/wetube/WetubeApplicationTests.java
@@ -0,0 +1,13 @@
+package me.weekbelt.wetube;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WetubeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
