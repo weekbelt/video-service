1758f48fc6a49560451364220e71ae4a74118528
weekbelt <vfrvfr4207@gmail.com>
2020-10-13 18:49:39 +0900

    feat: Create Comment Entity

---
+++ src/main/java/me/weekbelt/wetube/modules/comment/Comment.java
@@ -0,0 +1,28 @@
+package me.weekbelt.wetube.modules.comment;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import me.weekbelt.wetube.modules.BaseTimeEntity;
+import me.weekbelt.wetube.modules.member.Member;
+import me.weekbelt.wetube.modules.video.Video;
+
+import javax.persistence.*;
+
+@Getter @NoArgsConstructor
+@Entity
+public class Comment extends BaseTimeEntity {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 200, nullable = false)
+    private String text;
+
+    @ManyToOne
+    @JoinColumn(name = "video_id")
+    private Video video;
+
+    @ManyToOne
+    @JoinColumn(name = "member_id")
+    private Member member;
+}
