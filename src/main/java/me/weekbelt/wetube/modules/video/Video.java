8d8e27e5b0324bad9b3b3ce5d6a96dd208e20323
weekbelt <vfrvfr4207@gmail.com>
2020-10-15 00:55:19 +0900

    refactor: Auditing

--- src/main/java/me/weekbelt/wetube/modules/video/Video.java
+++ src/main/java/me/weekbelt/wetube/modules/video/Video.java
@@ -4,14 +4,17 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import me.weekbelt.wetube.modules.BaseTimeEntity;
+import me.weekbelt.wetube.modules.comment.Comment;
 import me.weekbelt.wetube.modules.member.Member;
 import me.weekbelt.wetube.modules.video.form.VideoUpdateForm;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter @NoArgsConstructor
 @Entity
-public class Video extends BaseTimeEntity {
+public class Video extends BaseTimeEntity{
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
