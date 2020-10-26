b0f9b9070f24051174409e0ce97585c14d772b9c
weekbelt <vfrvfr4207@gmail.com>
2020-10-26 18:20:52 +0900

    feat: Validate comment text

--- src/main/resources/static/js/addComment.js
+++ src/main/resources/static/js/addComment.js
@@ -1,20 +1,27 @@
 const commentObj = {
-    isValid: true,
+    isValid: false,
     currentMemberName: document.querySelector("#currentMemberName").value,
     init: function () {
+        // 댓글 글자 수 유효한지 확인하는 이벤트 등록
+        const commentText = document.querySelector("#newCommentText");
+        commentText.addEventListener("keyup", this.isValidText);
         // 댓글 작성 요청 이벤트 등록
         const commentCreateButton = document.querySelector("#commentAddBtn");
-        commentCreateButton.addEventListener("click", this.createCommitRequest);
-        // 댓글 시간 변환
+        commentCreateButton.addEventListener("click", this.createCommentRequest);
+        // 댓글 수정 요청 이벤트 등록
+        const commentModifyButton = document.querySelector("#commentModifyButton");
+        commentModifyButton.addEventListener("click", this.modfiyCommentRequest);
+        // 댓글 삭제 요청 이벤트 등록
+        const commentDeleteButton = document.querySelector("#replyDeleteButton");
+        commentDeleteButton.addEventListener("click", this.deleteCommentRequest);
 
     },
-    createCommitRequest: async function () {
+    createCommentRequest: async function () {
         const commentText = document.querySelector("#newCommentText");
         const createCommentForm = {
             text: commentText.value,
         }
 
-        // TODO: 3~200자 이하 검증
         if (!commentObj.isValid) {
             alert("글자수를 3자 이상 200자 이하로 입력해주세요.");
         } else {
@@ -24,9 +31,13 @@ const commentObj = {
 
             alert("등록 되었습니다.");
 
+            commentObj.isValid = false;
             commentText.value = "";
             commentObj.addCommentTemplate(response);
         }
+    },
+    modifyCommentRequest: function() {
+
     },
     addCommentTemplate: function (commentReadForm) {
         // 최근 수정 시간을 ~전 으로 처리
@@ -43,6 +54,12 @@ const commentObj = {
 
         const commentListContainer = document.querySelector("#commentList");
         commentListContainer.insertAdjacentHTML("afterbegin", commentElement);
+    },
+    isValidText: function (event) {
+        let text = event.target.value;
+        commentObj.isValid = (/^.{3,1000}$/).test(text);
+        
+        console.log(commentObj.isValid);
     }
 };
 
