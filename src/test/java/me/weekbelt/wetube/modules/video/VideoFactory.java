4aacb55f488ece77e3c0312f4ed331a3d075c3c4
weekbelt <vfrvfr4207@gmail.com>
2020-11-12 17:44:04 +0900

    refactor: VideoFactory

--- src/test/java/me/weekbelt/wetube/modules/video/VideoFactory.java
+++ src/test/java/me/weekbelt/wetube/modules/video/VideoFactory.java
@@ -1,6 +1,9 @@
 package me.weekbelt.wetube.modules.video;
 
 import lombok.RequiredArgsConstructor;
+import me.weekbelt.wetube.infra.util.FileUtils;
+import me.weekbelt.wetube.modules.FileInfo.FileInfo;
+import me.weekbelt.wetube.modules.FileInfo.repository.FileInfoRepository;
 import me.weekbelt.wetube.modules.member.Member;
 import me.weekbelt.wetube.modules.video.form.VideoUploadForm;
 import me.weekbelt.wetube.modules.video.repository.VideoRepository;
@@ -11,16 +14,31 @@ import org.springframework.stereotype.Component;
 public class VideoFactory {
 
     private final VideoRepository videoRepository;
+    private final FileInfoRepository fileInfoRepository;
 
-    public Video createVideo(Member member, VideoUploadForm videoUploadForm) {
+    public Video createVideo(Member member) {
+        FileInfo videoFile = FileInfo.builder()
+                .fileName("test.mp4")
+                .saveFileName("/upload_video")
+                .contentType("video/mp4")
+                .build();
+
+        FileInfo imageFile = FileInfo.builder()
+                .fileName("test.png")
+                .saveFileName("/upload_image")
+                .contentType("image/png")
+                .build();
+
+        FileInfo savedVideoFile = fileInfoRepository.save(videoFile);
+        FileInfo savedImageFile = fileInfoRepository.save(imageFile);
+
         Video video = Video.builder()
                 .member(member)
-                .title(videoUploadForm.getTitle())
-                .description(videoUploadForm.getDescription())
+                .title("test video")
+                .description("test video description")
                 .views(0L)
-                // TODO: video 처리
-                .videoSaveFileName(videoUploadForm.getVideoFile().getOriginalFilename())
-                .thumbnailSaveFileName(videoUploadForm.getThumbnailImageFile().getOriginalFilename())
+                .videoFile(savedVideoFile)
+                .imageFile(savedImageFile)
                 .build();
 
         return videoRepository.save(video);
