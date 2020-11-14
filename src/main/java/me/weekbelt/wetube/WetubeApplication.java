fb4c04c0acd42c2cc36cdbccf156f06a53edb7fe
weekbelt <vfrvfr4207@gmail.com>
2020-11-14 18:52:34 +0900

    refactor: Input init data

--- src/main/java/me/weekbelt/wetube/WetubeApplication.java
+++ src/main/java/me/weekbelt/wetube/WetubeApplication.java
@@ -6,14 +6,15 @@ import me.weekbelt.wetube.modules.member.Member;
 import me.weekbelt.wetube.modules.member.form.MemberJoinForm;
 import me.weekbelt.wetube.modules.member.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
-public class WetubeApplication implements CommandLineRunner {
+public class WetubeApplication {
 
     @Autowired
     private MemberService memberService;
@@ -24,8 +25,9 @@ public class WetubeApplication implements CommandLineRunner {
         SpringApplication.run(WetubeApplication.class, args);
     }
 
-    @Override
-    public void run(String... args) throws Exception {
+    @EventListener(ApplicationReadyEvent.class)
+    public void init() {
+
         MemberJoinForm memberJoinForm = new MemberJoinForm();
         memberJoinForm.setName("Skywalker");
         memberJoinForm.setEmail("vfrvfr4207@gmail.com");
@@ -34,10 +36,9 @@ public class WetubeApplication implements CommandLineRunner {
         Member member = memberService.processNewMember(memberJoinForm);
 
         // 배포시 지워야 할 데이터
-        for(int i=1; i<=27; i++) {
+        for (int i = 1; i <= 27; i++) {
             CommentCreateForm commentCreateForm = CommentCreateForm.builder().text("댓글 남겨요." + i).build();
             commentService.addComment(1L, member.getId(), commentCreateForm);
         }
-
     }
 }
